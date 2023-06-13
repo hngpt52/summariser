520e0ddb95cefc588f5f417bee8c4a8b37fcc0d2
HamzaNT <hamza.ntwari@napier.ai>
2023-06-14 00:30:07 +0100

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,34 @@
-import {logo} from "../assets";
+import React from "react";
 
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
-        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
-          <button
-          type="button"
-          onClick={() => window.open("https://github.com/hngpt52/summariser")}  
-          className="black_btn"
-          >GitHub</button>
-      </nav>
-    </header>
-  )
-}
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
 
-export default Hero
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/hngpt52/summariser", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
